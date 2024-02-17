57b8afb7a4f86ee487906c8ba51017a72c6dfeee
Adam <adamgerhant@gmail.com>
2024-02-17 00:36:01 -0800

    support for obj files without vertex normals

--- src/resources.rs
+++ src/resources.rs
@@ -112,7 +112,6 @@ pub async fn load_model(
         
         }
     
-
         //info!("vertices: {:#?}",vertices);
         //info!("indices: {:#?}", indices);
 
@@ -137,75 +136,7 @@ pub async fn load_model(
             material: 0,
         };
         
-        /*
-        let centroid_vertices: Vec<(f32, f32, f32)> = stl.vertices.iter()
-            .map(|v| {            
-                (v[0], v[1], v[2])
-            })
-            .collect();
-        let centroid = calculate_centroid(&centroid_vertices);
-        let offset = (-centroid.0, -centroid.1, -centroid.2);
-        let max_dimension = calculate_max_dimension(&centroid_vertices);
         
-        // Determine the scaling factor needed to achieve the desired height
-        let desired_height = 6.0; // Example desired height
-        let scaling_factor = desired_height / max_dimension;
-
-        let vertices = stl
-            .vertices
-            .iter()
-            .enumerate()
-            .map(|(i, vertex)| {
-                //check if normal data exists
-                let normal = if i < stl.faces.len() {
-                    (stl.faces[i].normal).into()
-                } else {
-                    [0.0, 0.0, 0.0] 
-                };
-                //info!("vertex {} position: {:#?}", i, vertex);
-                let offset_vertex = [
-                    (vertex[0] + offset.0)*scaling_factor,
-                    (vertex[1] + offset.1)*scaling_factor,
-                    (vertex[2] + offset.2)*scaling_factor,
-                ];
-                //info!("offet vertex {} position: {:#?}", i, offset_vertex);
-
-                model::ModelVertex {
-                    position: offset_vertex,//(*vertex).into()
-                    tex_coords: [0.0, 0.0],
-                    normal,
-                }
-            })
-            .collect::<Vec<_>>();
-            
-
-        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some(&format!("STL Vertex Buffer")),
-            contents: bytemuck::cast_slice(&vertices),
-            usage: wgpu::BufferUsages::VERTEX,
-        });
-
-        let mut indices = Vec::new();
-
-        for face in &stl.faces {
-            let vertex_indices = [face.vertices[0], face.vertices[1], face.vertices[2]];
-            indices.extend_from_slice(&vertex_indices);
-        }
-
-        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some(&format!("STL Index Buffer")),
-            contents: bytemuck::cast_slice(&indices),
-            usage: wgpu::BufferUsages::INDEX,
-        });
-
-        let mesh_model = model::Mesh {
-            name: "STL model".to_string(),
-            vertex_buffer,
-            index_buffer,
-            num_elements: indices.len() as u32,
-            material: 0,
-        };
-        */
         meshes = vec![mesh_model];
 
         
@@ -267,20 +198,34 @@ pub async fn load_model(
             .map(|m| {
                 
                 let vertices = (0..m.mesh.positions.len() / 3)
-                    .map(|i| model::ModelVertex {
-                        position: [
-                            m.mesh.positions[i * 3],
-                            m.mesh.positions[i * 3 + 1],
-                            m.mesh.positions[i * 3 + 2],
-                        ],
-                        tex_coords: [m.mesh.texcoords[i * 2], 1.0 - m.mesh.texcoords[i * 2 + 1]],
-                        normal: [
-                            m.mesh.normals[i * 3],
-                            m.mesh.normals[i * 3 + 1],
-                            m.mesh.normals[i * 3 + 2],
-                        ],
-                    })
-                    .collect::<Vec<_>>();
+                .map(|i| {
+                    if m.mesh.normals.is_empty(){
+                        model::ModelVertex {
+                            position: [
+                                m.mesh.positions[i * 3],
+                                m.mesh.positions[i * 3 + 1],
+                                m.mesh.positions[i * 3 + 2],
+                            ],
+                            tex_coords: [m.mesh.texcoords[i * 2], 1.0 - m.mesh.texcoords[i * 2 + 1]],
+                            normal: [0.0, 0.0, 0.0],
+                        }
+                    }else{
+                        model::ModelVertex {
+                            position: [
+                                m.mesh.positions[i * 3],
+                                m.mesh.positions[i * 3 + 1],
+                                m.mesh.positions[i * 3 + 2],
+                            ],
+                            tex_coords: [m.mesh.texcoords[i * 2], 1.0 - m.mesh.texcoords[i * 2 + 1]],
+                            normal: [
+                                m.mesh.normals[i * 3],
+                                m.mesh.normals[i * 3 + 1],
+                                m.mesh.normals[i * 3 + 2],
+                            ],
+                        }
+                    }
+                })
+                .collect::<Vec<_>>();
 
 
 
